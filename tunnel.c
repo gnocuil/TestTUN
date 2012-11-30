9239c1bc35abaccbeb55e8b17e49b9e2cad8bb56
gnocuil <gnocuil@gmail.com>
2012-12-01 00:50:58 +0800

    fixed route & changed mtu

--- tunnel.c
+++ tunnel.c
@@ -4,13 +4,44 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
-#include <linux/if_tun.h>
+
 #include <sys/types.h>
 #include <errno.h>
 #include <net/route.h>
 
+#ifdef __linux
+#include <linux/if_tun.h>
+#elif __APPLE__
+//TODO : mac os
+#else
+//TODO : unknown os
+#endif
+
 #include <tunnel.h>
 
+//ifconfig interface_name mtu xxx
+static int set_mtu(char *interface_name, unsigned mtu)
+{
+    int fd;
+    if ((fd = socket(PF_INET,SOCK_STREAM,0)) < 0) {
+        printf("Error create socket :%m\n", errno);
+        return -1;
+    }
+
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name,interface_name);
+    ifr.ifr_mtu = mtu;
+
+    if(ioctl(fd, SIOCSIFMTU, &ifr) < 0)
+    {
+        printf("Error up %s :%m\n",interface_name, errno);
+        return -1;
+    }
+
+    return 0;
+}
+
+//ifconfig interface_name up
 static int interface_up(char *interface_name) 
 {
     int s;
@@ -40,7 +71,6 @@ static int interface_up(char *interface_name)
     }
 
     return 0;
-
 }
 /*
 int set_ipaddr(char *interface_name, char *ip)
@@ -103,81 +133,87 @@ static int tun_create(char *dev, int flags)
     return fd;
 }
 
-/**
- *  route add 0.0.0.0/0 dev interface_name
- */
-int route_add(char * interface_name)
+//ip route add default dev interface_name
+static int route_add(char * interface_name)
 {
     int skfd;
     struct rtentry rt;
 
     struct sockaddr_in dst;
-    struct sockaddr_in gw;
+    //struct sockaddr_in gw;
     struct sockaddr_in genmask;
+
+    bzero(&genmask,sizeof(struct sockaddr_in));
+    genmask.sin_family = AF_INET;
+    genmask.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+    bzero(&dst,sizeof(struct sockaddr_in));
+    dst.sin_family = AF_INET;
+    dst.sin_addr.s_addr = inet_addr("0.0.0.0");
 
     memset(&rt, 0, sizeof(rt));
 
-    genmask.sin_addr.s_addr = inet_addr("255.0.0.0");
+    rt.rt_dst = *(struct sockaddr*) &dst;
+    rt.rt_genmask = *(struct sockaddr*) &genmask;
 
-    bzero(&dst,sizeof(struct sockaddr_in));
-    dst.sin_family = PF_INET;
-    dst.sin_addr.s_addr = inet_addr("0.0.0.0");
+    skfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(ioctl(skfd, SIOCDELRT, &rt) < 0) 
+    {
+        //printf("Error route del :%m\n", errno);
+        //return -1;
+    }
+
+    memset(&rt, 0, sizeof(rt));
 
     rt.rt_metric = 0;
+  
     rt.rt_dst = *(struct sockaddr*) &dst;
     rt.rt_genmask = *(struct sockaddr*) &genmask;
 
     rt.rt_dev = interface_name;
-    rt.rt_flags = RTF_UP | RTF_HOST ;
+    rt.rt_flags = RTF_UP;
 
-    skfd = socket(AF_INET, SOCK_DGRAM, 0);
+    //skfd = socket(AF_INET, SOCK_DGRAM, 0);
     if(ioctl(skfd, SIOCADDRT, &rt) < 0) 
     {
         printf("Error route add :%m\n", errno);
         return -1;
     }
 }
+
+
 int main(int argc, char *argv[])
 {
-#ifdef __linux
-	puts("Running in linux!");
-#elif __APPLE__
-	puts("Running in mac os!");
-#else
-	puts("unknown OS!");
-	return 1;
-#endif
-    int tun, ret;
-    char tun_name[IFNAMSIZ];
+    char tun_name[IFNAMSIZ] = "lw4over6";
     unsigned char buf[4096];
     unsigned char ip[4];
 
-    tun_name[0] = '\0';
-    tun = tun_create(tun_name, IFF_TUN | IFF_NO_PI);
+    int tun = tun_create(tun_name, IFF_TUN | IFF_NO_PI);
     if (tun < 0) 
     {
         return 1;
     }
     printf("TUN name is %s\n", tun_name);
 
-    //激活虚拟网卡增加到虚拟网卡的路由
-    interface_up(tun_name);
+    set_mtu(tun_name, 50000);//set mtu to 50000
+    interface_up(tun_name);//interface up
     //set_ipaddr(tun_name, "10.0.0.1");
     route_add(tun_name);
 
+    int len;
     while (1) {//printf("loop!\n");
 
-        ret = read(tun, buf, sizeof(buf));
+        len = read(tun, buf, sizeof(buf));
         
-        printf("read %d bytes\n", ret);
+        printf("read %d bytes\n", len);
         int i;
-        for(i=0;i<ret;i++)
+        for(i=0;i<len;i++)
         {
           printf("%02x ",buf[i]);
         }
         printf("\n");
         
-        if (ret < 0)
+        if (len < 0)
             break;
 #define ETH_LEN 0
         memcpy(ip, &buf[ETH_LEN + 12], 4);
@@ -186,7 +222,7 @@ int main(int argc, char *argv[])
         buf[ETH_LEN + 20] = 0;
         *((unsigned short*)&buf[ETH_LEN + 22]) += 8;
         
-        ret = write(tun, buf, ret);
+        len = write(tun, buf, len);
         //printf("write %d bytes\n", ret);
     }
 
